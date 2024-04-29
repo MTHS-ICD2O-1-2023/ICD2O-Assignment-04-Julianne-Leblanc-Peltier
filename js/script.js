2576193b119e603b1eb8cb27f31fd969625004b6
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-04-29 18:43:23 +0000

    js edited + added elements

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // variables
+  const alwaysOnButtonChecked = document.getElementById("option-positive").checked
+  const TAX = 0.13
+  let priceOfBurger = 0
+
+  if (alwaysOnButtonChecked == true) {
+    // process
+    let priceOfBurger = priceOfBurger + 5.0
+  } else {
+    // process
+    let priceOfBurger = priceOfBuger = 7.50
+  }
+
+  if (alwaysOnButtonChecked2 == true) {
+    // process
+    let priceOfBurger = priceOfBurger + 1.20
+  }
+  else if (OnButtonCheck == true) {
+    .// process
+    let priceOfBurger = priceOfBurger + 2.50
+  } else {
+    // process
+    let priceOfBurger = priceOfBurger + 3.75
+  }
+  // output
+  document.getElementById("end-price").innerHTML = "Your total is: $" + priceOfBurger + (priceOfBurger * TAX)
+}
+
+*/
